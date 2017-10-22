44b36da9139e8fe5541f613f4a321717e9424d3b
angelyactayosanchez <1523353@utp.edu.pe>
2017-10-21 20:48:02 -0500

    BD script

---
+++ Datos_BD_DWI_falta.sql
@@ -0,0 +1,13 @@
+use dw;
+
+insert into Contact(Name,Phone,Email) values ('Juan Dominguez',99784356,'luis95@gmail.com');
+insert into Contact(Name,Phone,Email) values ('Ana Paz',913147754,'anaPaz_35@hotmail.com');
+insert into Contact(Name,Phone,Email) values ('Pedro Silva',947635892,'pedro_123@gmail.com');
+insert into Contact(Name,Phone,Email) values ('Angel Yactayo',963541208,'angelyacto18@gmail.com');
+insert into Contact(Name,Phone,Email) values ('Flor Salas',936504107,'flor23Salas@outlook.com');
+select * from Contact;
+use dw;/*
+insert into Notification(Title,Timeto,Description) values();
+select*from Notification;*/
+describe Products;
+insert into Products(Name,Price,Picture,Description)values();
